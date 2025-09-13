871b93777bb89cd276d861313faa55a53a318deb
Robin Škába <robin.skaba@gmail.com>
2025-09-13 11:36:04 +0200

    removed unused

--- lib/model/calculator/calculation_exceptions.dart
+++ lib/model/calculator/calculation_exceptions.dart
@@ -2,12 +2,6 @@ class InvalidHexCharacterException implements Exception {}
 
 class InvalidHexNumberException implements Exception {}
 
-class InvalidExpressionException implements Exception {}
-
-class SimpleExpressionIsNotSimpleException implements Exception {}
-
-class LoadingBufferBeganAtBracketException implements Exception {}
-
 class PrecisionShouldNotBeOver20Exception implements Exception {}
 
 class CanNotConvertIntegerOver15ToHexCharacterException implements Exception {}
